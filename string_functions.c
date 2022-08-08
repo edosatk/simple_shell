bc721b47ca2814e0fa741228234ef00835f3c70f
edosatk <gendabusen@gmail.com>
2022-08-07 20:52:40 -0700

    string functions used

---
+++ string_functions.c
@@ -0,0 +1,99 @@
+#include "shell.h"
+/**
+ *_strcpy - copies a string to another buffer
+ *@source: source to copy from
+ *@dest: destination to copy to
+ *
+ * Return: void
+ */
+
+void _strcpy(char *source, char *dest)
+{
+	int i = 0;
+
+	for (; source[i] != '\0'; i++)
+		dest[i] = source[i];
+	dest[i] = '\0';
+}
+
+/**
+ *_strlen - counts string length
+ *@string: string to be counted
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *string)
+{
+	int len = 0;
+
+	if (string == NULL)
+		return (len);
+	for (; string[len] != '\0'; len++)
+		;
+	return (len);
+}	
+/**
+ *_strcmp - compare two strings
+ *@first: first string to be compared
+ *@second: second string to be compared
+ *
+ * Return: difference of the two strings
+ */
+
+int _strcmp(char *first, char *second)
+{
+	int i = 0;
+
+	while (first[i] != '\0')
+	{
+		if (first[i] != second[i])
+			break;
+		i++;
+	}
+	return (first[i] - second[i]);
+}
+
+/**
+ *_strcat - concatenates two strings
+ *@destination: string to be concatenated to
+ *@source:  string to concatenate
+ *
+ * Return: address of the new string
+ */
+
+char *_strcat(char *destination, char *source)
+{
+	char *new_string =  NULL;
+	int len_dest = _strlen(destination);
+	int len_source = _strlen(source);
+
+	new_string = malloc(sizeof(*new_string) * (len_dest + len_source + 1));
+	_strcpy(destination, new_string);
+	_strcpy(source, new_string + len_dest);
+	new_string[len_dest + len_source] = '\0';
+	return (new_string);
+}
+
+/**
+ *_strspn - gets the length of a prefix substring
+ *@str1: string to be searched
+ *@str2: string to be used
+ *
+ *Return: number of bytes in the initial segment of 5 which are part of accept
+ */
+
+int _strspn(char *str1, char *str2)
+{
+	int i = 0;
+	int match = 0;
+
+	while (str1[i] != '\0')
+	{
+		if (_strchr(str2, str1[i]) == NULL)
+			break;
+		match++;
+		i++;
+	}
+	return (match);
+}
